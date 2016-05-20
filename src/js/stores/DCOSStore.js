4c9f7c7958962e1665ed78887a69f6e1c4c6965d
Pierluigi Cau <pierlo@mesosphere.io>
2016-05-20 14:47:56 +0200

    Introduce store and struct placeholders for Jobs

--- src/js/stores/DCOSStore.js
+++ src/js/stores/DCOSStore.js
@@ -13,6 +13,7 @@ import ChronosStore from '../stores/ChronosStore';
 import Framework from '../structs/Framework';
 import MarathonStore from './MarathonStore';
 import MesosSummaryStore from './MesosSummaryStore';
+import JobTree from '../structs/JobTree';
 import ServiceTree from '../structs/ServiceTree';
 import SummaryList from '../structs/SummaryList';
 
@@ -40,6 +41,7 @@ class DCOSStore extends EventEmitter {
         queue: new Map(),
         versions: new Map()
       },
+      chronos: new JobTree(),
       mesos: new SummaryList(),
       dataProcessed: false
     };
@@ -254,6 +256,12 @@ class DCOSStore extends EventEmitter {
       );
     });
 
+  }
+
+  get jobTree() {
+    let {chronos} = this.data;
+
+    return chronos;
   }
 
   get dataProcessed() {
