3f9fd9568fa85721cde370662fa5cb755e3fe0de
mlunoe <michael.lunoe@gmail.com>
2016-06-09 15:07:41 -0400

    Add TaskStatsTable

--- src/js/components/ServiceDetailDebugTab.js
+++ src/js/components/ServiceDetailDebugTab.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import DescriptionList from './DescriptionList';
 import Service from '../structs/Service';
+import TaskStatsTable from './TaskStatsTable';
 
 class ServiceDetailDebugTab extends React.Component {
   getValueText(value) {
@@ -70,6 +71,18 @@ class ServiceDetailDebugTab extends React.Component {
     return (<DescriptionList hash={LastVersionChangeValueMapping} />);
   }
 
+  getTaskStats() {
+    let {taskStats} = this.props.service;
+
+    if (taskStats == null || Object.keys(taskStats).length === 0) {
+      return (
+        <p>This app does not have task statistics</p>
+      );
+    }
+
+    return <TaskStatsTable taskStats={taskStats} />
+  }
+
   render() {
     return (
       <div>
@@ -81,6 +94,10 @@ class ServiceDetailDebugTab extends React.Component {
           Last Task Failure
         </h5>
         {this.getLastTaskFailureInfo()}
+        <h5 className="inverse flush-top">
+          Task Statistics
+        </h5>
+        {this.getTaskStats()}
       </div>
     );
   }
